f80cd4b5bb487df6fa8c1b28bd44be4a93da8659
Kumar Anshuman <115361246+Anshuman6484@users.noreply.github.com>
2024-09-11 13:40:49 +0000

    conversionOperations

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -46,15 +46,17 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);    // 12
+// console.log(1 + "2");    // 12
+// console.log("1" + 2 + 2);    // 122
+// console.log(1 + 2 + "2");    // 32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+// "" is 0, '+' converts the boolean into number
 
 let num1, num2, num3
 
